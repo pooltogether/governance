d9a1f78b640ba76de503f0aa21a8fde09375b442
Aodhgan <aodhgan@pooltogether.com>
2021-01-26 16:09:10 -0800

    added employee contracts

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -11,9 +11,29 @@ function green() {
 module.exports = async (hardhat) => {
   const { getNamedAccounts, deployments, ethers } = hardhat
   const { deploy } = deployments
-  const { deployer } = await getNamedAccounts()
+  const { deployer,
+          merkleDistributor,
+          employeeA,
+          employeeB,
+          employeeC,
+          employeeD,
+          employeeL,
+          employeeLi
+ } = await getNamedAccounts()
+ 
+ 
+ const signers = await ethers.getSigners()
 
-  dim(`Deployer is ${deployer}`)
+ dim(`Deployer is ${deployer}`)
+ 
+
+ const retroDistibutionTotalAmount = 1.5e24 // 1.5 million
+
+
+
+ const allEmployees = {employeeA, employeeB, employeeC, employeeD, employeeL, employeeLi}
+//  const employeePercentage =
+
 
   // only mint five minutes after deployment
   const mintAfter = parseInt(new Date().getTime() / 1000) + 300
@@ -28,6 +48,38 @@ module.exports = async (hardhat) => {
     // skipIfAlreadyDeployed: true
   })
   green(`Deployed DefiSaver token: ${defiSaverResult.address}`)
+
+  // wait until mintAfter has expired
+
+
+  // deploy investor and employee Treasury contracts
+  allEmployees.forEach((employee)=>{
+    dim("deploying Treasury contract for : ", employeeA)
+    const vestingAmount = 100 // todo populate from percentage array
+    const twoYearsInSecondsUnix = mintAfter + 63072000 
+
+    const treasuryResult = await deploy('TreasuryVester', {
+    args: [
+      desfiSaverResult.address,
+      employee,
+      vestingAmount,
+      mintAfter,
+      0,
+      twoYearsInSecondsUnix
+    ],
+    from: deployer,
+    skipIfAlreadyDeployed: true
+    })
+    green(`Deployed TreasuryVesting for ${employee} at contract: ${treasuryResult.address}`)
+    
+    // now mint to each Treasury contract
+    const defiSaver = await ethers.getContractAt('DefiSaver', defiSaverResult.address, deployerSigner)
+    const mintToTreasuryResult = await defiSaver.mint(treasuryResult.address, retroDistibutionTotalAmount)
+
+
+  })
+
+
 
   const governorResult = await deploy('GovernorAlpha', {
     contract: 'GovernorZero',
@@ -51,11 +103,31 @@ module.exports = async (hardhat) => {
   })
   green(`Deployed Timelock: ${timelockResult.address}`)
 
-  const signers = await ethers.getSigners()
+  
   const governor = await ethers.getContractAt('GovernorAlpha', governorResult.address, signers[0])
 
   dim(`Setting timelock...`)
   await governor.setTimelock(timelockResult.address)
 
+
+
+  // wait for mint period to expire and then mint tokens for MerkleDistributor TODO with setTimeout()
+  // mint tokens for merkleDistributor
+  const deployerSigner =  signers[0]
+  const defiSaver = await ethers.getContractAt('DefiSaver', defiSaverResult.address, deployerSigner)
+  const mintTokensToMerkleDistributorResult = await defiSaver.mint(merkleDistributor, retroDistibutionTotalAmount)
+  green(`Minted tokens to MerkleDistributor`)
+  
+
+
   green(`Done!`)
 };
+
+
+function checkFlag() {
+  if(flag == false) {
+    setTimeout(checkFlag, 100); /* this checks the flag every 100 milliseconds*/
+  } else {
+
+  }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -26,7 +26,39 @@ module.exports = {
   namedAccounts: {
     deployer: {
       default: 0
+    },
+    merkleDistributor:{
+      4: "0x0c6271c65DA3183A2e7a0914c840a6F0a04D050d"
+    },
+    employeeB :{
+      1: "0xa38445311cCd04a54183CDd347E793F4D548Df3F",
+      4: "0xa38445311cCd04a54183CDd347E793F4D548Df3F"
+    },
+    employeeL :{
+      1: "0xa5c3A513645A9a00cB561fED40438E9DFE0D6a69",
+      4: "0xa5c3A513645A9a00cB561fED40438E9DFE0D6a69"
+    },
+    employeeD:{
+      1: "0xe507F2d7dE97c783a60FeF9f1c4A4dade2b0a989",
+      4: "0xe507F2d7dE97c783a60FeF9f1c4A4dade2b0a989"
+    },
+    employeeLi:{
+      1: "0xEc0286a4B478ECd600d3D96E398157B4825C5a38",
+      4: "0xEc0286a4B478ECd600d3D96E398157B4825C5a38"
+    },
+    employeeC:{
+      1: "0x5Ff0F990137ED250c84C492a896cB3F980D0f6B9",
+      4: "0x5Ff0F990137ED250c84C492a896cB3F980D0f6B9"
+    },
+    employeeA:{
+      1: "0xB3152182472ba2E46B11C75440a72D087F0750B6",
+      4: "0xB3152182472ba2E46B11C75440a72D087F0750B6"
+    },
+    investor1:{
+      1: "0x",
+      4: "0x"
     }
+    
   },
   networks
 };
